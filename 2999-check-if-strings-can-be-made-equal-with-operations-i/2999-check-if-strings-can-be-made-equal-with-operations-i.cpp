8313eb4d7f4b0fba0f036966c3ee7d8e19eb471b
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-09-07 00:39:06 +0530

    Time: 3 ms (36.64%), Space: 6 MB (27.17%) - LeetHub

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     bool canBeEqual(string s1, string s2) 
     {
-        for( int i =0;i<s1.length();i++)
-        {
-            if(s1[i]!=s2[i] && i<2)
-            {
-                if(s1[i]==s2[i+2] && s1[i+2]==s2[i])
-                 {
-                    swap(s1[i],s1[i+2]);
-                 }
-                else
-                 return false; 
-            }
-            if(s1[i]!=s2[i] && i>1)
-            return false;
-        }
-        return true;
+        bool even = ( s1[0]==s2[0] && s1[2]==s2[2] ) ||
+                    ( s1[0]==s2[2] && s1[2]==s2[0] );
+        bool odd = ( s1[1]==s2[1] && s1[3]==s2[3] ) ||
+                   ( s1[1]==s2[3] && s1[3]==s2[1] );
+        return even && odd;
     }
 };
