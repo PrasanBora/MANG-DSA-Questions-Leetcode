ce8e003f7769e8034a96a45f46ebd380f9955b61
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-09-10 01:09:49 +0530

    Time: 3 ms (50.38%), Space: 6.7 MB (46.62%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -18,7 +18,21 @@ public:
     }
     int combinationSum4(vector<int>& nums, int target)
     {
-        vector<int>dp(target+1,-1);
-        return solve(nums,target,dp);
+        // vector<int>dp(target+1,-1);
+        // return solve(nums,target,dp);
+
+        vector< unsigned int>dp(target+1,0);
+        dp[0]=1;
+        for(int i =1;i<=target;i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if( i-nums[j] >=0)
+                {
+                    dp[i]+=dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
     }
 };
