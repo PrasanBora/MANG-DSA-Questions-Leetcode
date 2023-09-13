05a6ff1f570ec8063099be9e1cd49495917bd238
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-09-13 22:32:22 +0530

    Time: 0 ms (100%), Space: 6.9 MB (5.1%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -5,14 +5,11 @@ public:
         int len=0;
         int i = s.length()-1;
         while(s[i]==' ')
-        {
-            i--;
-        }
-        while(  i>=0 && s[i]!=' ')
-        {
+         i--;
+
+        while(  i>=0 && s[i--]!=' ')
             len++;
-            i--;
-        }
+
         return len;
     }
 };
