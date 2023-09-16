8fa032b732f9c2945edf24a2febfac438f8e1f87
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-09-16 21:55:35 +0530

    Time: 184 ms (12.5%), Space: 164.1 MB (87.5%) - LeetHub

--- 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.cpp
+++ 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+ /*Intuition
+We store the count of the element that occurs maximum number of times.
+
+Notice that if the count of max occuring element is less than n/2 times, then the element can be cancelled by remaining elements. In this case, if array size is even, then we say that ans is 0 and 1 incase the array size is odd. Because after cancelling all elements, 1 element is still remaining at the end.
+
+If the element occurs more than n/2 times, so now it cannot cancel all elements. But it will still cancel all remaining elements.
+How many elements are remaining? The elements remaining are (n - maxi). So elements left are maxi - (n - maxi) = 2*maxi - n;
+*/
     int minLengthAfterRemovals(vector<int>& nums) 
     {
       int n=nums.size();
