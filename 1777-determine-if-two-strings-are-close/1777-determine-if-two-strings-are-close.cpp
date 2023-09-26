9a9e14b3ec00633fc699492ffcedfbd84f720c9b
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-09-27 02:16:27 +0530

    Time: 103 ms (53%), Space: 20.8 MB (35.04%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -5,19 +5,30 @@ public:
         if(word1.size()!=word2.size())
          return false;
 
-        unordered_map<char,int>mp1,mp2;
+        // unordered_map<char,int>mp1,mp2;
+
+        // for(auto it :word1)
+        //  mp1[it]++;
+
+        // for( auto it:word2)
+        //  mp2[it]++;
+
+        // for( auto it: mp1)
+        //  {
+        //      if(mp2.find(it.first)==mp2.end())
+        //       return false;
+        //  }
+
+        unordered_map<char,int>mp1;
 
         for(auto it :word1)
          mp1[it]++;
 
         for( auto it:word2)
-         mp2[it]++;
-
-        for( auto it: mp1)
-         {
-             if(mp2.find(it.first)==mp2.end())
-              return false;
-         }
+         mp1.erase(it);
+         
+        if(!mp1.empty())
+         return false; 
 
         vector<int>v1(26,0);
         vector<int>v2(26,0);
@@ -26,14 +37,12 @@ public:
         {
            v1[word1[i]-'a']++;
            v2[word2[i]-'a']++;
-        //    cout<<v1[word1[i]-'a']<<endl;
         }
         sort(v1.begin(),v1.end());
         sort(v2.begin(),v2.end());
 
         for(int i=0;i<26;i++)
         {
-            // cout<<v1[i]<<"  "<<v2[i]<<endl;
             if(v1[i]!=v2[i])
              return false;
         }
