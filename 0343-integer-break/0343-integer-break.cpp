b5f593287a20c37efa5a8fc1d1f6d0085798a971
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-07 00:50:11 +0530

    Time: 2 ms (49.96%), Space: 6.7 MB (17.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp)
-    {
-        if(n<=1)
-         return 1;
-        if(dp[n]!=-1)
-         return dp[n];
+    // int solve(int n,vector<int>&dp)
+    // {
+    //     if(n<=1)
+    //      return 1;
+    //     if(dp[n]!=-1)
+    //      return dp[n];
 
-        int mx=1;
-        for( int i=1;i<n;i++)
-        {
-          mx= max(mx,max(i*(n-i),i*solve(n-i,dp)));
-        } 
-        return dp[n]=mx;
-    }
+    //     int mx=1;
+    //     for( int i=1;i<n;i++)
+    //     {
+    //       mx= max(mx,max(i*(n-i),i*solve(n-i,dp)));
+    //     } 
+    //     return dp[n]=mx;
+    // }
 
-    int solvetb(int n)
-    {
-        vector<int>dp(n+1,0);
-         dp[1]=1;
+    // int solvetb(int n)
+    // {
+    //     vector<int>dp(n+1,0);
+    //      dp[1]=1;
         
-        for(int i=2;i<=n;i++)
-        {
-            for( int j=1;j<i;j++)
-                dp[i]= max ( dp[i],max(j*(i-j),j*dp[i-j]));
-        }
-        return dp[n];
+    //     for(int i=2;i<=n;i++)
+    //     {
+    //         for( int j=1;j<i;j++)
+    //             dp[i]= max ( dp[i],max(j*(i-j),j*dp[i-j]));
+    //     }
+    //     return dp[n];
 
-    }
+    // }
 
     int solvemath(int n)
     {
