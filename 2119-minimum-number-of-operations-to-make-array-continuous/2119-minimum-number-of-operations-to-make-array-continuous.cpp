7ee4b6fb277483ddce0353b51c66a13cb76e04e4
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-10 18:22:39 +0530

    Time: 137 ms (93.1%), Space: 64.6 MB (71.55%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size()-1;
         int l =0;
-        int count=0,maxi=0;
+        int maxi=0;
 
         for(int i=0;i<nums.size();i++)
         {
@@ -18,12 +18,9 @@ public:
         for(int i=0;i<l;i++)
         {
             while(r<l && (nums[r]-nums[i] <=n))
-            {
-                count++;
                 r++;
-            }
-            maxi =max(maxi,count);
-            count--;
+            
+            maxi =max(maxi,r-i);
         }
         return nums.size()-maxi;
     }
