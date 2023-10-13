5d93d76e338b3b6ab598a1dc2ddf5e678c1b64ed
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-14 00:33:00 +0530

    Time: 0 ms (100%), Space: 14 MB (37.3%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -11,17 +11,33 @@ public:
 
     // }
 
-     int solve( vector<int>&cost,int n,vector<int>&dp)
+    //  int solve( vector<int>&cost,int n,vector<int>&dp)
+    // {
+    //     if(n==0)
+    //      return cost[0];
+    //     if(n==1)
+    //      return cost[1];
+
+    //     if(dp[n]!=-1)
+    //      return dp[n]; 
+
+    //     return dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+
+    // }
+
+    int solve( vector<int>&cost)
     {
-        if(n==0)
-         return cost[0];
-        if(n==1)
-         return cost[1];
+        int n=cost.size();
+        vector<int>dp(cost.size()+1,0);
+         dp[0]= cost[0];
+         dp[1]= cost[1];
 
-        if(dp[n]!=-1)
-         return dp[n]; 
+        for(int i=2;i<cost.size();i++)
+        {
+          dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        } 
 
-        return dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return min(dp[n-1],dp[n-2]);
 
     }
 
@@ -30,9 +46,11 @@ public:
         // return min (solve(cost,cost.size()-1) ,
         //             solve(cost,cost.size()-2));
 
-        vector<int>dp(cost.size()+1,-1);
-        return min (solve(cost,cost.size()-1,dp) ,
-                    solve(cost,cost.size()-2,dp));
+        // vector<int>dp(cost.size()+1,-1);
+        // return min (solve(cost,cost.size()-1,dp) ,
+        //             solve(cost,cost.size()-2,dp));
+
+        return solve(cost);
 
     }
 };
