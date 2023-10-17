ea5b9835ddd81f994c32dc73cc27e6b1302bb25e
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-18 00:57:02 +0530

    Time: 36 ms (70.97%), Space: 32.6 MB (83.28%) - LeetHub

---
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    
+    bool binarytree( int root,vector<int>& leftchild, vector<int>& rightchild)
+    {
+        // bfs
+         vector<int>vis(leftchild.size(),0);
+         queue<int>q;
+
+         q.push(root);
+         vis[root]=true;
+
+         while(!q.empty())
+         {
+             int curr=q.front();
+             q.pop();
+
+             if(leftchild[curr]!=-1)
+             {
+                 if(vis[leftchild[curr]])
+                  return false;
+
+                 q.push(leftchild[curr]);
+                 vis[leftchild[curr]]=true;
+
+             }
+
+             if(rightchild[curr]!=-1)
+             {
+                 if(vis[rightchild[curr]])
+                  return false;
+
+                 q.push(rightchild[curr]);
+                 vis[rightchild[curr]]=true;
+
+             }
+         }
+         for( auto it : vis )
+         {
+             if(!it)
+              return false;
+         }
+         return true;
+    }
+    
+    bool validateBinaryTreeNodes(int n, vector<int>& leftchild, vector<int>& rightchild) 
+    {
+        // cycle detection
+        vector<bool>count(n,0);
+        for( int i=0;i<n;i++)
+        {
+            if(leftchild[i]!=-1)
+             {
+               if(count[leftchild[i]])
+                return false;
+               count[leftchild[i]]=true; 
+             }
+
+            if(rightchild[i]!=-1)
+             {
+               if(count[rightchild[i]])
+                return false;
+               count[rightchild[i]]=true;
+             } 
+        }
+
+        // single root 
+
+        int root=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(!count[i])
+             {
+                 if(root==-1)
+                 root=i;
+                 else 
+                  return false;
+             }
+        }
+        if(root==-1)
+         return false;
+
+        return binarytree(root,leftchild,rightchild) ;
+    }
+};
