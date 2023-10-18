3a00838cf3ced5297eca98d67fb51c9e2dba31e6
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-18 21:39:35 +0530

    Time: 136 ms (72.72%), Space: 116 MB (40.85%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -6,7 +6,7 @@ public:
          return 0;
 
         if(it==cost.size())
-         return 1e9;
+         return 1e7;
 
         if(dp[it][remain]!=-1)
          return dp[it][remain];
@@ -18,10 +18,34 @@ public:
 
         return dp[it][remain];
     }
+
+    int solvetb( vector<int>& cost, vector<int>& time )
+    {
+        int n=cost.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=n;i++)
+            dp[n][i]=1e9;
+        for(int it=n-1;it>=0;it--)
+        { 
+          for(int remain=1;remain<=n;remain++ )
+          {
+            int paint = cost[it]+ dp[it+1][max(0,remain-1-time[it])];
+            int notpaint =  dp[it+1][remain];
+
+            dp[it][remain] = min(paint,notpaint);
+          }
+        }
+
+        return dp[0][n];
+    }
+
     int paintWalls(vector<int>& cost, vector<int>& time) 
     {
-        int n= cost.size();
-      vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       return solve(0,n,cost,time,dp)   ;
+        // int n= cost.size();
+    //   vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //    return solve(0,n,cost,time,dp)   ;
+
+         return solvetb(cost,time);
     }
 };
