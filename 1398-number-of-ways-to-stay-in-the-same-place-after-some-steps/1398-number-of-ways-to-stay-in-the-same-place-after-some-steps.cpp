f1cbc48325607a962140822bd477525fd62f020f
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-18 20:43:15 +0530

    Time: 63 ms (5.68%), Space: 13.1 MB (40.18%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -3,6 +3,7 @@ class Solution
 public:
     int MOD = 1e9+7 ;
     int n;
+
     int solve( int steps,int curr,vector<vector<int>>&dp)
     {
         if(steps==0)
@@ -25,11 +26,37 @@ public:
 
         return dp[steps][curr];   
     }
+
+     int solvetb( int steps,int arrLen)
+    {
+        vector<vector<int>>dp(steps+1,vector<int>(arrLen+1,0));
+        dp[0][0]=1;
+        for( int st=1;st<=steps;st++)
+        {
+            for( int curr=arrLen-1;curr>=0;curr--)
+            {
+                
+               dp[st][curr]=dp[st-1][curr]%MOD; 
+              if(curr>0)
+               dp[st][curr]+=dp[st-1][curr-1]%MOD;
+               
+               dp[st][curr]=dp[st][curr]%MOD;
+              if(curr < arrLen-1)
+                dp[st][curr]+=dp[st-1][curr+1]%MOD;
+
+                dp[st][curr]=dp[st][curr]%MOD;
+            }
+        }
+        
+        return dp[steps][0];   
+    }
+
     int numWays(int steps, int arrLen) 
     {
         arrLen =min(steps,arrLen);
         n=arrLen;
-        vector<vector<int>>dp(steps+1,vector<int>(steps+1,-1));
-        return solve( steps,0,dp);
+        // vector<vector<int>>dp(steps+1,vector<int>(steps+1,-1));
+         return solvetb(steps,arrLen);
+        // return solve( steps,0,dp);
     }
 };
