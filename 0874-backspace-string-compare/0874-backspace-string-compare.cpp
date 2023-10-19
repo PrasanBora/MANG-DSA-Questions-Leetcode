47fc234be021958697bc5ffa673dc0074ec45476
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-19 10:16:04 +0530

    Time: 0 ms (100%), Space: 6.5 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -27,24 +27,61 @@ public:
         // t =simplify(t);
 
         // return s==t;
-         
-         string temp1;
-         string temp2;
-        for( int i=0;i<s.length();i++)
-        {
-           if(s[i]!='#')
-            temp1.push_back(s[i]);
-           else 
-             if(temp1.length())temp1.pop_back(); 
-        }
-        for( int i=0;i<t.length();i++)
-        {
-           if(t[i]!='#')
-             temp2.push_back(t[i]);
-           else 
-             if(temp2.length())temp2.pop_back();
-        }
+//----------------------------------------------------
+        //  string temp1;
+        //  string temp2;
+        // for( int i=0;i<s.length();i++)
+        // {
+        //    if(s[i]!='#')
+        //     temp1.push_back(s[i]);
+        //    else 
+        //      if(temp1.length())temp1.pop_back(); 
+        // }
+        // for( int i=0;i<t.length();i++)
+        // {
+        //    if(t[i]!='#')
+        //      temp2.push_back(t[i]);
+        //    else 
+        //      if(temp2.length())temp2.pop_back();
+        // }
+        // return temp1==temp2;
 
-        return temp1==temp2;
+  //--------------------------------------------
+       int i=s.length()-1;
+       int j=t.length()-1;
+       int skipS=0;
+       int skipT=0;
+
+       while(i>=0||j>=0)
+       {
+           while(i>=0)
+           {
+               if(s[i]=='#')
+                skipS++,i--;
+               else if(skipS>0) 
+                skipS--,i--;
+               else 
+               break; 
+           }
+
+           while(j>=0)
+           {
+               if(t[j]=='#')
+                skipT++,j--;
+               else if(skipT>0) 
+                skipT--,j--;
+               else 
+               break; 
+           }
+           if(i<0 &&j<0)
+            return true;
+           if(i<0||j<0) return false;
+           else if(s[i]!=t[j])
+             return false; 
+
+            i--;
+            j--; 
+       }      
+       return true;
     }
 };
