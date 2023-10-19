82729a485670d7e68de56e94450a828d6ea4e4e8
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-19 09:49:28 +0530

    Time: 0 ms (100%), Space: 6.6 MB (39.46%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
-     string simplify(string str)
-     {
-         stack<char>st;
-         for(int i=0;i<str.length();i++)
-         {
-             if(str[i]=='#')
-             {
-               if(st.empty()) continue;
-               else st.pop();
-             }
-             else 
-              st.push(str[i]); 
-         }
-         string ans="";
+    //  string simplify(string str)
+    //  {
+    //      stack<char>st;
+    //      for(int i=0;i<str.length();i++)
+    //      {
+    //          if(str[i]=='#')
+    //          {
+    //            if(st.empty()) continue;
+    //            else st.pop();
+    //          }
+    //          else 
+    //           st.push(str[i]); 
+    //      }
+    //      string ans="";
 
-         while(!st.empty())
-          {ans = st.top()+ans;
-          st.pop();}
+    //      while(!st.empty())
+    //       {ans = st.top()+ans;
+    //       st.pop();}
 
-         return ans; 
-     }
+    //      return ans; 
+    //  }
     bool backspaceCompare(string s, string t) 
     {
-        s = simplify(s);
-        t =simplify(t);
+        // s = simplify(s);
+        // t =simplify(t);
 
-        return s==t;
+        // return s==t;
+         
+         string temp1;
+         string temp2;
+        for( int i=0;i<s.length();i++)
+        {
+           if(s[i]!='#')
+            temp1.push_back(s[i]);
+           else 
+             if(temp1.length())temp1.pop_back(); 
+        }
+        for( int i=0;i<t.length();i++)
+        {
+           if(t[i]!='#')
+             temp2.push_back(t[i]);
+           else 
+             if(temp2.length())temp2.pop_back();
+        }
+
+        return temp1==temp2;
     }
 };
