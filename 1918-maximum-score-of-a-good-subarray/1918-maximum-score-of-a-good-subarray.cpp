e2bb93f336e61bf7dc96bc0a0817e1501a70ee45
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-22 23:52:52 +0530

    Time: 121 ms (90.57%), Space: 89.6 MB (80.92%) - LeetHub

--- 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -43,22 +43,47 @@ public:
     //     cout<<nums[i]<<" ";
     // } 
 
+    // int maximumScore(vector<int>& nums, int k) 
+    // {
+    //     int n=nums.size();
+    //     vector<int>left = get_left_min(nums,n);
+    //     vector<int>right = get_right_min(nums,n);
+    //     int ans= 0;
+        
+    //     // print(left);
+    //     // cout<<endl;
+    //     // print(right);
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(left[i]<k && right[i]>k )
+    //          ans= max(ans,nums[i]*(right[i]-left[i]-1));
+    //     }
+    //     return ans;
+    // }
+
     int maximumScore(vector<int>& nums, int k) 
     {
         int n=nums.size();
-        vector<int>left = get_left_min(nums,n);
-        vector<int>right = get_right_min(nums,n);
-        int ans= 0;
+        int res = nums[k];
+        int mini = nums[k];
+        int i=k;
+        int j=k;
         
-        // print(left);
-        // cout<<endl;
-        // print(right);
-
-        for(int i=0;i<n;i++)
+        while(i>0 || j<n-1)
         {
-            if(left[i]<k && right[i]>k )
-             ans= max(ans,nums[i]*(right[i]-left[i]-1));
+            if(i==0)
+             j++;
+            else if( j==n-1)
+             i--;
+            else if( nums[i-1]<nums[j+1])
+             j++;
+            else 
+             i--;
+
+            mini = min(mini,min(nums[i],nums[j]));
+            res = max(res,mini*(j-i+1)); 
         }
-        return ans;
+        return res;
     }
 };
