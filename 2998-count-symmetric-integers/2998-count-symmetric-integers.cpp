60e9e271f6808db4089c3e730cf30008690ce386
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-23 01:49:28 +0530

    Time: 34 ms (76.41%), Space: 6.6 MB (15.21%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -7,23 +7,21 @@ public:
        {
            int left =0;
            int right =0;
-
-           
-              string temp = to_string(i); 
+           string temp = to_string(i); 
               if(temp.size()%2==0)
-              {int n=temp.size()/2;
-              for(int i=0;i<temp.size()/2;i++)
-               {
+              {
+                  int n=temp.size()/2;
+                  for(int i=0;i<temp.size()/2;i++)
+                  {
                    left +=temp[i]-'0';
                    right +=temp[n+i]-'0';
-               }
+                  }
                 if( left==right)
                    {
-                      cout<< left<<" "<<right<<endl;
+                    //   cout<< left<<" "<<right<<endl;
                        ans++;
                    }
                }
-           
        }   
         return ans;
     }
