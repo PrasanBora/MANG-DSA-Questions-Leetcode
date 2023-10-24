f9cf9f5d859eb31460faf50ac465124d81eeb8aa
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-10-24 16:43:57 +0530

    Time: 8 ms (68.56%), Space: 22.4 MB (63%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -11,34 +11,52 @@
  */
 class Solution {
 public:
+    vector<int>ans;
+    void dfs(TreeNode*node,int depth)
+    {
+        if( node==NULL)
+         return ;
+
+        if( depth==ans.size())
+         ans.push_back(node->val);
+
+        else 
+         ans[depth] =max( ans[depth],node->val);
+
+         dfs(node->left,depth+1);
+         dfs(node->right,depth+1);  
+    }
     vector<int> largestValues(TreeNode* root) 
     {
-        if(root==NULL)
-         return vector<int>{};
+        // if(root==NULL)
+        //  return vector<int>{};
 
-        vector<int> ans;
-        queue<TreeNode*>q;
-        q.push(root);
+        // vector<int> ans;
+        // queue<TreeNode*>q;
+        // q.push(root);
 
-        while(!q.empty())
-        {
-            int sz= q.size();
-            int cmax =INT_MIN;
+        // while(!q.empty())
+        // {
+        //     int sz= q.size();
+        //     int cmax =INT_MIN;
 
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
+        //     for(int i=0;i<sz;i++)
+        //     {
+        //         TreeNode* node = q.front();
+        //         q.pop();
 
-                cmax=max(cmax,node->val);
+        //         cmax=max(cmax,node->val);
 
-                if(node->left)
-                 q.push(node->left);
-                if(node->right)
-                 q.push(node->right); 
-            }
-            ans.push_back(cmax);
-        }
-        return ans;
+        //         if(node->left)
+        //          q.push(node->left);
+        //         if(node->right)
+        //          q.push(node->right); 
+        //     }
+        //     ans.push_back(cmax);
+        // }
+        // return ans;
+
+         dfs(root,0);
+         return ans;
     }
 };
