29ec3b3c55ec3529a11f340f5c287dbb1f70a560
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-11-01 15:23:58 +0530

    Time: 45 ms (26.51%), Space: 30.6 MB (23.61%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -17,32 +17,56 @@ public:
 
     // }
 
-    int solvemem( string s,string t,int is,int it,
-      vector<vector<int>>&dp)
-    {
-        if(it==t.size())
-         return 1;
-        if(is>=s.size())
-         return 0;
+    // int solvemem( string s,string t,int is,int it,
+    //   vector<vector<int>>&dp)
+    // {
+    //     if(it==t.size())
+    //      return 1;
+    //     if(is>=s.size())
+    //      return 0;
 
-        if(dp[is][it]!=-1)
-         return dp[is][it]; 
+    //     if(dp[is][it]!=-1)
+    //      return dp[is][it]; 
 
-        if(s[is]==t[it])
-         {
-             return dp[is][it]=solvemem(s,t,is+1,it+1,dp)+solvemem(s,t,is+1,it,dp);
-         } 
-         else 
-          return dp[is][it]=solvemem(s,t,is+1,it,dp);
+    //     if(s[is]==t[it])
+    //      {
+    //          return dp[is][it]=solvemem(s,t,is+1,it+1,dp)+solvemem(s,t,is+1,it,dp);
+    //      } 
+    //      else 
+    //       return dp[is][it]=solvemem(s,t,is+1,it,dp);
 
-    }
+    // }
 
     int numDistinct(string s, string t) 
     {
 
         // return solve(s,t,0,0);
-        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,-1));
-        return solvemem(s,t,0,0,dp);
+        // vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,-1));
+        // return solvemem(s,t,0,0,dp);
+         
 
+         // using 1 based indexing 
+        // dp[i][j] is representing the no. of subseq of t[0-it] in s[0-is]
+        vector<vector<long long>>dp(s.size()+1,vector<long long>(t.size()+1,0));
+
+        for(int i=0;i<=s.size();i++)
+         dp[i][0]=1;
+
+        // an empty string is a subseq of each  
+         const int MOD =1e9+7;
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=1;j<=t.size();j++)
+            {
+                if(s[i-1]==t[j-1])
+                 dp[i][j]=dp[i-1][j-1]%MOD+dp[i-1][j]%MOD;
+                else
+                 dp[i][j]=dp[i-1][j]%MOD;   
+            }
+        }
+        if (dp[s.size()][t.size()] >=INT_MAX)
+         return -1;
+        else 
+         return dp[s.size()][t.size()];  
     }
 };
