a6a6287bd5bc83219ed5cd16b2b6b6566e6c0670
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-11-01 11:28:29 +0530

    Time: 0 ms (100%), Space: 6.6 MB (55.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -21,34 +21,61 @@ public:
 
     // }
 
-    int solvemem( int index,string s,vector<int>&dp)
-    {
-      if( index==s.size())
-       return 1;
+    // int solvemem( int index,string s,vector<int>&dp)
+    // {
+    //   if( index==s.size())
+    //    return 1;
 
-      if(dp[index]!=-1)
-       return dp[index]; 
+    //   if(dp[index]!=-1)
+    //    return dp[index]; 
       
-      int one =0;
-       if(s[index]!='0')
-         one = solvemem( index+1,s,dp);
+    //   int one =0;
+    //    if(s[index]!='0')
+    //      one = solvemem( index+1,s,dp);
 
-      int two =0;
-       if( s[index]!='0' && index +1<s.size())
-        {
-            int temp = stoi(s.substr(index,2));
-            if( temp<=26 && temp>0)
-            two = solvemem(index+2,s,dp);
-        }
+    //   int two =0;
+    //    if( s[index]!='0' && index +1<s.size())
+    //     {
+    //         int temp = stoi(s.substr(index,2));
+    //         if( temp<=26 && temp>0)
+    //         two = solvemem(index+2,s,dp);
+    //     }
     
-     return dp[index] = one+two;
+    //  return dp[index] = one+two;
 
-    }
+    // }
+
+    // int numDecodings(string s) 
+    // {
+    //     vector<int>dp(s.length()+1,-1);
+    //     // return solve(0,s);
+    //     return solvemem(0,s,dp);
+    // }
 
     int numDecodings(string s) 
     {
-        vector<int>dp(s.length()+1,-1);
-        // return solve(0,s);
-        return solvemem(0,s,dp);
+        vector<int>dp(s.length()+1);
+         dp[0]=1;
+        if(s[0]=='0')
+         dp[1]=0;
+        else 
+         dp[1]=1;
+
+        for(int i=2;i<=s.size();i++)
+        {
+            int one,two;
+            if(s[i-1]=='0')
+             one=0;
+            else 
+             one =dp[i-1]; 
+
+            if(stoi(s.substr(i-2,2))<=26 &&  stoi(s.substr(i-2,2))>0 && s[i-2]!='0')
+             two =dp[i-2];
+            else 
+              two=0;
+
+              dp[i] = one +two; 
+        } 
+        return dp[s.size()];
     }
 };
