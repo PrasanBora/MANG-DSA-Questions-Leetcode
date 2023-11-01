ec66b5dbaec91f15fe375e1e1f954ce199276d17
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-11-01 10:15:37 +0530

    Time: 2 ms (49.55%), Space: 8 MB (11.69%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    // int solve( int index,string s)
+    // {
+    //   if( index==s.size())
+    //    return 1;
+      
+    //   int one =0;
+    //    if(s[index]!='0')
+    //      one = solve( index+1,s);
+
+    //   int two =0;
+    //    if( index+1<s.size() &&
+    //        (s[index]=='1'||s[index]=='2') 
+    //        && s[index+1]<='6')
+    //        {
+    //           two = solve(index+2,s);
+    //        }
+    
+    //  return one+two;
+
+    // }
+
+    int solvemem( int index,string s,vector<int>&dp)
+    {
+      if( index==s.size())
+       return 1;
+
+      if(dp[index]!=-1)
+       return dp[index]; 
+      
+      int one =0;
+       if(s[index]!='0')
+         one = solvemem( index+1,s,dp);
+
+      int two =0;
+       if( index+1<s.size() &&
+           (s[index]=='1'||s[index]=='2' 
+           && s[index+1]<='6'))
+           {
+              two = solvemem(index+2,s,dp);
+           }
+    
+     return dp[index] = one+two;
+
+    }
+
+    int numDecodings(string s) 
+    {
+        vector<int>dp(s.length()+1,-1);
+        // return solve(0,s);
+        return solvemem(0,s,dp);
+    }
+};
