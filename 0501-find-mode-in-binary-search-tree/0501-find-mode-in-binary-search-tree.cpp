6d575cfe5b5a93396c40722eac0bec748ec749fe
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2023-11-01 09:17:20 +0530

    Time: 14 ms (75.21%), Space: 24.3 MB (91.46%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,33 +11,77 @@
  */
 class Solution {
 public:
-    map<int,int>freq;
-    void getfreq(TreeNode * root)
-    {
-        if(!root)
-        return;
-        freq[root->val]++;
-        getfreq(root->left);
-        getfreq(root->right);
-    }
-    vector<int> findMode(TreeNode* root)
-    {
-       if(!root)
-       return {};
-       vector<int> res; 
-       getfreq(root); 
-       int mxf =-1;
-       for( auto e:freq)
-       {
-           if(e.second==mxf)
-               res.push_back(e.first);
-           else if(e.second>mxf)
-           {
-               mxf=e.second;
-               res.clear();
-               res.push_back(e.first);
-           }
-       }
-       return res;
-    }
+
+    // unordered_map<int,int>freq;
+    // void get_freq(TreeNode* root)
+    // {
+    //     if(!root)
+    //      return;
+    //     freq[root->val]++;
+    //     get_freq(root->left);
+    //     get_freq(root->right);
+    // }
+
+    // vector<int> findMode(TreeNode* root) 
+    // {
+    //     if(!root)
+    //      return{};
+
+    //     vector<int>res;
+    //     get_freq(root);
+        
+    //     int max_freq=-1;
+    //     for( auto it:freq)
+    //     {
+    //         if(it.second>max_freq)
+    //         {
+    //             max_freq=it.second;
+    //             res.clear();
+    //             res.push_back(it.first);
+    //         }
+    //         else if( it.second==max_freq)
+    //           res.push_back(it.first);
+
+    //     }
+    //     return res;
+    // }
+        
+        int currNum,maxStreak,currStreak;
+        vector<int>res;
+
+        void inorder(TreeNode* root)
+        {
+            if(!root)
+             return ;
+
+            inorder(root->left);
+
+            int num = root->val;
+            if(num==currNum)
+             currStreak++;
+            else 
+              {
+                  currStreak=1;
+                  currNum=num;
+              } 
+
+            if(currStreak>maxStreak)
+            {
+                res={};
+                maxStreak=currStreak;
+            }  
+            if( currStreak==maxStreak)
+            {
+                res.push_back(num);
+            }
+
+            inorder(root->right);
+        }
+        vector<int> findMode(TreeNode* root) 
+        {
+            if(!root)
+             return {};
+            inorder(root);
+            return res; 
+        }
 };
