9661095b5822a8917dfb178f2e96a67adb7ec482
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2024-03-12 02:14:07 +0530

    Time: 50 ms (44.56%), Space: 30 MB (77.11%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,19 +1,36 @@
+// class Solution {
+// public:
+//     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) 
+//     {
+//         map<int,vector<int>>mp;
+        
+//       for( int i = 0;i<score.size();i++)
+//       {
+//           mp[score[i][k]] = score[i]; 
+//       }
+        
+//         int n = score.size()-1;
+        
+//         for( auto it:mp)
+//         score[n--] =  it.second;
+        
+//         return score;
+//     }
+// };
+
+int k1;
+     bool comp(vector<int>&a,vector<int>&b)
+    {
+      return a[k1]>b[k1];  
+    }
+
 class Solution {
 public:
-    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) 
-    {
-        map<int,vector<int>>mp;
-        
-      for( int i = 0;i<score.size();i++)
-      {
-          mp[score[i][k]] = score[i]; 
-      }
-        
-        int n = score.size()-1;
-        
-        for( auto it:mp)
-        score[n--] =  it.second;
-        
+   
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        k1=k;    
+        sort(score.begin(),score.end(),comp);
         return score;
+        
     }
 };
