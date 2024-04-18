36a2876e82f6f6ab54f883b3e78fda638f16de22
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2024-04-19 00:20:06 +0530

    Time: 57 ms (95.80%), Space: 100.8 MB (76.64%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-   int islandPerimeter(vector<vector<int>>& grid) {
-        int count=0, repeat=0;
-        for(int i=0;i<grid.size();i++)
+    int islandPerimeter(vector<vector<int>>& grid) 
+    {
+        int land =0 ,common=0;
+        for( int i =0 ;i<grid.size();i++)
         {
-            for(int j=0; j<grid[i].size();j++)
-                {
-                    if(grid[i][j]==1)
-                    {
-                        count ++;
-                        if(i!=0 && grid[i-1][j] == 1) repeat++;
-                        if(j!=0 && grid[i][j-1] == 1) repeat++;
-                    }
-                }
+            for( int j =0;j<grid[0].size();j++)
+            {
+              if( grid[i][j] == 1)
+               {   
+                  land ++;
+                
+                if( i >0 && grid[i-1][j] == 1)
+                    common++;
+                if( j >0 && grid[i][j-1] == 1)
+                    common++;
+              }
+            }
         }
-        return 4*count-repeat*2;
+        return 4*land - ( 2* common);
     }
 };
