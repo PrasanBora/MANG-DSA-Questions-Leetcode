c50b09d3b323c78499dcb88c083771c6439e1a66
PrasanBora <100477554+PrasanBora@users.noreply.github.com>
2024-04-25 23:04:15 +0530

    Time: 1289 ms (25.85%), Space: 279.8 MB (7.60%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    
+    bool dfs(unordered_map<int,vector<int>> &graph,int start,int end,vector<bool>&visited)
+    {
+        if( start == end )
+            return true;
+        
+        visited[start] = true ;
+        
+        for( auto adj : graph[start])
+        {
+            if( visited[adj] == false )
+            {
+                if( dfs(graph,adj,end,visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
     bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
         unordered_map<int,vector<int>> graph; 
         for(auto e : edges) {
@@ -7,22 +26,7 @@ public:
             graph[e[1]].push_back(e[0]);
         }
         vector<bool> visited(n,0);        
-        stack<int> st;
-        st.push(start);
-        visited[start] = 1; 
-        
-        while(!st.empty()){ 
-            auto top = st.top();
-            if(top == end)
-                return 1;
-            st.pop();
-            for(auto node : graph[top]){
-                if(!visited[node]){
-                    visited[node] = 1;
-                    st.push(node); 
-                }
-            }
-        }
-        return false;
+       
+        return dfs(graph,start,end,visited);
     }
 };
