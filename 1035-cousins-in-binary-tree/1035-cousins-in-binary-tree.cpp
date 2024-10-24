01eaf53efe1925203c684b6254ecfff15c826d14
Prasan Bora <100477554+PrasanBora@users.noreply.github.com>
2024-10-24 09:22:11 +0530

    Time: 1 ms (51.15%), Space: 13.1 MB (95.93%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
@@ -1,35 +1,41 @@
 
 class Solution {
 public:
-    //  function to find the depth and parent of a given node value
-    void findDepthAndParent(TreeNode* node, TreeNode* parent, int depth, int val, int& nodeDepth, TreeNode*& nodeParent) {
-        if(node == NULL) return; // base case   
-        // If the current node matches the value we're looking for
-        if (node->val == val){// current node == x or current node == y 
-            nodeDepth = depth; // Set the depth of the node
-            nodeParent = parent; // Set the parent of the node
-            return; // Return as we found the node
+    int findDepthAndParent(TreeNode* node, TreeNode* parent, int depth, int val, TreeNode*& nodeParent)
+    {
+        if(node == NULL) 
+          return -1000; 
+
+        if (node->val == val)
+        {
+            nodeParent = parent; 
+            return depth ; 
         }
-        // recur for the left child
-        findDepthAndParent(node->left, node, depth + 1, val, nodeDepth, nodeParent);
-        // recur for the right child
-        findDepthAndParent(node->right, node, depth + 1, val, nodeDepth, nodeParent);
+     
+       int left_find = findDepthAndParent(node->left, node, depth + 1, val, nodeParent);
+
+       if( left_find != -1000 )
+        return left_find ;
+       
+       else return 
+        findDepthAndParent(node->right, node, depth + 1, val, nodeParent);
     }
 
-    // Main function to determine if two nodes are cousins
     bool isCousins(TreeNode* root, int x, int y) 
     {
         if (root==NULL) 
-        return false; 
+         return false; 
 
-        int xDepth = -1;
-        int yDepth = -1; // Initialize depths of x and y
-        TreeNode* xParent = NULL; //  parent of x
-        TreeNode* yParent = NULL; //  parent of y
+        TreeNode* xParent = NULL; 
+        TreeNode* yParent = NULL; 
         
-        findDepthAndParent(root, NULL, 0, x, xDepth, xParent);
-        findDepthAndParent(root, NULL, 0, y, yDepth, yParent);
-       
+        int xDepth = findDepthAndParent(root, NULL, 0, x, xParent);
+        int yDepth = findDepthAndParent(root, NULL, 0, y, yParent);
+
+        cout<< xParent <<" -- "<<yParent<<endl;
+
         return (xDepth == yDepth) && (xParent != yParent);
     }
+    
 };
+
